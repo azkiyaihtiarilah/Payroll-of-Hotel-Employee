1c2e79023ad5aa4cec6e416a86de8026eb44170e
Kiki0122 <145021163+Kiki0122@users.noreply.github.com>
2023-11-07 10:41:47 +0700

    Update Selection1Student.java

--- Jobsheet5/Selection1Student.java
+++ Jobsheet5/Selection1Student.java
@@ -3,15 +3,37 @@ import java.util.Scanner;
 public class Selection1Student {
 
     public static void main(String[] args) {
-        Scanner input01 = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("Input a Number = ");
-        int number = input01.nextInt();
+        System.out.print("Enter a Number: ");
+        int number = input.nextInt();
 
-        if(number%2 == 0){
-            System.out.println(number+" is an even number!");
-        }else{
-            System.out.println(number+ "is an odd number!");
+        boolean isEven = (number % 2 == 0);
+        boolean isPrime = isPrime(number);
+
+        if (isEven) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
         }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
